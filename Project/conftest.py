3ecbf4a850a2cddde83b38c93505a8f618810452
Максим Бова <mbova@web100-sumy.local>
2020-07-23 18:34:37 +0300

    conftest file and others improvements

---
+++ Project/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+import time
+from Project.common.factories.driver_factory import DriverFactory
+
+
+@pytest.fixture(scope="function")
+def driver():
+    driver_factory = DriverFactory("chrome")
+    driver = driver_factory.get_driver()
+    yield driver
+    driver.quit()
