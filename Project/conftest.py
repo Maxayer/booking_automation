488d0cccc2122d56e5239c25e9dd43ef475611b6
Максим Бова <mbova@web100-sumy.local>
2020-07-24 15:03:20 +0300

    now you can choose browser for running tests in command line, see in README file

--- Project/conftest.py
+++ Project/conftest.py
@@ -3,9 +3,14 @@ import time
 from Project.common.factories.driver_factory import DriverFactory
 
 
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="opera",
+                     help="Choose browser: chrome, opera or firefox")
+
 @pytest.fixture(scope="function")
-def driver():
-    driver_factory = DriverFactory("opera")
+def driver(request):
+    browser_name = request.config.getoption("browser_name")
+    driver_factory = DriverFactory(browser_name)
     driver = driver_factory.get_driver()
     yield driver
     driver.quit()
---
+++ README.txt
@@ -0,0 +1,15 @@
+the project allows to commit tests on three browsers: chrome, firefox and opera
+
+launch the tests from root directory, for exemple:
+
+pytest -v -s Project/tests/test_class.py
+
+this command launches the tests in Google Chrome
+
+if you want to see the tests running in another browser, note this in the line:
+
+pytest -s -v --browser_name=opera Project/tests/test_class.py
+
+or:
+
+pytest -s -v --browser_name=firefox Project/tests/test_class.py
